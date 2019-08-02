ed2265aa56d8c106e258bd27086bdb3ec0fa6bfe
Damien Trouillet <damien.trouillet@caissedesdepots.fr>
2019-08-02 15:20:36 +0200

    Pass context to plugin manager

--- src/main/java/fr/icdc/ebad/config/PluginConfiguration.java
+++ src/main/java/fr/icdc/ebad/config/PluginConfiguration.java
@@ -3,11 +3,20 @@ package fr.icdc.ebad.config;
 import org.pf4j.spring.SpringPluginManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.GenericWebApplicationContext;
 
 @Configuration
 public class PluginConfiguration {
+    private final GenericWebApplicationContext context;
+
+    public PluginConfiguration(GenericWebApplicationContext context) {
+        this.context = context;
+    }
+
     @Bean
     public SpringPluginManager pluginManager() {
-        return new SpringPluginManager();
+        SpringPluginManager pluginManager = new SpringPluginManager();
+        pluginManager.setApplicationContext(context);
+        return pluginManager;
     }
 }
