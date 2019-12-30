49a8e65957361c09a6f9fd8f3015ba781b0b7594
Damien Trouillet <damien.trouillet@caissedesdepots.fr>
2019-12-30 14:19:04 +0100

    Add unit test norms resource

--- src/test/java/fr/icdc/ebad/web/rest/NormResourceTest.java
+++ src/test/java/fr/icdc/ebad/web/rest/NormResourceTest.java
@@ -2,7 +2,8 @@ package fr.icdc.ebad.web.rest;
 
 import fr.icdc.ebad.config.Constants;
 import fr.icdc.ebad.domain.Norme;
-import fr.icdc.ebad.repository.NormeRepository;
+import fr.icdc.ebad.service.NormeService;
+import fr.icdc.ebad.util.TestUtil;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,10 +25,13 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.only;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -42,7 +46,7 @@ public class NormResourceTest {
     private NormResource normResource;
 
     @MockBean
-    private NormeRepository normeRepository;
+    private NormeService normeService;
 
     private MockMvc restMvc;
 
@@ -71,7 +75,7 @@ public class NormResourceTest {
         Page<Norme> normePage = new PageImpl<>(normeList);
         MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/norms");
 
-        when(normeRepository.findAll(any(Pageable.class))).thenReturn(normePage);
+        when(normeService.getAllNormes(any(Pageable.class))).thenReturn(normePage);
 
         restMvc.perform(builder)
                 .andExpect(status().isOk())
@@ -80,7 +84,7 @@ public class NormResourceTest {
                 .andExpect(jsonPath("$.content[0].id", is(1)))
                 .andExpect(jsonPath("$.content[1].id", is(2)));
 
-        verify(normeRepository, only()).findAll(any(Pageable.class));
+        verify(normeService, only()).getAllNormes(any(Pageable.class));
     }
 
     @Test
@@ -99,7 +103,7 @@ public class NormResourceTest {
         Page<Norme> normePage = new PageImpl<>(normeList);
         MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/norms/name");
 
-        when(normeRepository.findAll(any(Pageable.class))).thenReturn(normePage);
+        when(normeService.getAllNormes(any(Pageable.class))).thenReturn(normePage);
 
         restMvc.perform(builder)
                 .andExpect(status().isOk())
@@ -108,27 +112,147 @@ public class NormResourceTest {
                 .andExpect(jsonPath("$.content[0].id", is(1)))
                 .andExpect(jsonPath("$.content[1].id", is(2)));
 
-        verify(normeRepository, only()).findAll(any(Pageable.class));
+        verify(normeService, only()).getAllNormes(any(Pageable.class));
     }
 
     @Test
+    @WithMockUser(roles = {"ADMIN"})
     public void getOne() throws Exception {
 
+        Norme norme1 = new Norme();
+        norme1.setId(1L);
+        norme1.setPathShell("/test");
+        norme1.setName("nameNorme");
+        norme1.setCommandLine("/bin/bash");
+        norme1.setCtrlMDate("date.txt");
+
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/norms/1");
+
+        when(normeService.findNormeById(eq(1L))).thenReturn(Optional.of(norme1));
+
+        restMvc.perform(builder)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.name", is(norme1.getName())))
+                .andExpect(jsonPath("$.pathShell", is(norme1.getPathShell())))
+                .andExpect(jsonPath("$.commandLine", is(norme1.getCommandLine())))
+                .andExpect(jsonPath("$.ctrlMDate", is(norme1.getCtrlMDate())));
     }
 
     @Test
+    @WithMockUser(roles = {"ADMIN"})
     public void save() throws Exception {
+        Norme norme = Norme.builder()
+                .pathShell("/test")
+                .name("nameNorme")
+                .commandLine("/bin/bash")
+                .ctrlMDate("date.txt")
+                .build();
+
+        Norme normWithId = Norme.builder()
+                .id(1L)
+                .pathShell("/test")
+                .name("nameNorme")
+                .commandLine("/bin/bash")
+                .ctrlMDate("date.txt")
+                .build();
+
+        when(normeService.saveNorme(argThat(thatNorm ->
+                norme.getPathShell().equals(thatNorm.getPathShell())
+                        && norme.getCtrlMDate().equals(thatNorm.getCtrlMDate())
+                        && norme.getName().equals(thatNorm.getName())
+                        && norme.getCommandLine().equals(thatNorm.getCommandLine())
+        ))).thenReturn(normWithId);
+
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.put("/norms");
+        restMvc.perform(
+                builder
+                        .contentType(TestUtil.APPLICATION_JSON_UTF8)
+                        .content(TestUtil.convertObjectToJsonBytes(norme)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.name", is(normWithId.getName())))
+                .andExpect(jsonPath("$.pathShell", is(normWithId.getPathShell())))
+                .andExpect(jsonPath("$.commandLine", is(normWithId.getCommandLine())))
+                .andExpect(jsonPath("$.ctrlMDate", is(normWithId.getCtrlMDate())));
+
+        verify(normeService).saveNorme(eq(norme));
+    }
+
+    @Test
+    @WithMockUser(roles = {"ADMIN"})
+    public void saveError() throws Exception {
+        Norme norme = Norme.builder()
+                .id(1L)
+                .build();
+
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.put("/norms");
+        restMvc.perform(
+                builder
+                        .contentType(TestUtil.APPLICATION_JSON_UTF8)
+                        .content(TestUtil.convertObjectToJsonBytes(norme)))
+                .andExpect(status().isBadRequest());
 
     }
 
     @Test
+    @WithMockUser(roles = {"ADMIN"})
     public void delete() throws Exception {
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.delete("/norms/1");
+        restMvc.perform(
+                builder)
+                .andExpect(status().isOk());
+
+        verify(normeService).deleteNormeById(eq(1L));
 
     }
 
     @Test
+    @WithMockUser(roles = {"ADMIN"})
     public void update() throws Exception {
+        Norme norme = Norme.builder()
+                .id(1L)
+                .pathShell("/test")
+                .name("nameNorme")
+                .commandLine("/bin/bash")
+                .ctrlMDate("date.txt")
+                .build();
 
+
+        when(normeService.saveNorme(argThat(thatNorm ->
+                norme.getPathShell().equals(thatNorm.getPathShell())
+                        && norme.getCtrlMDate().equals(thatNorm.getCtrlMDate())
+                        && norme.getName().equals(thatNorm.getName())
+                        && norme.getCommandLine().equals(thatNorm.getCommandLine())
+        ))).thenReturn(norme);
+
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.patch("/norms");
+        restMvc.perform(
+                builder
+                        .contentType(TestUtil.APPLICATION_JSON_UTF8)
+                        .content(TestUtil.convertObjectToJsonBytes(norme)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.name", is(norme.getName())))
+                .andExpect(jsonPath("$.pathShell", is(norme.getPathShell())))
+                .andExpect(jsonPath("$.commandLine", is(norme.getCommandLine())))
+                .andExpect(jsonPath("$.ctrlMDate", is(norme.getCtrlMDate())));
+
+        verify(normeService).saveNorme(eq(norme));
+    }
+
+    @Test
+    @WithMockUser(roles = {"ADMIN"})
+    public void updateError() throws Exception {
+        Norme norme = Norme.builder()
+                .build();
+
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.patch("/norms");
+        restMvc.perform(
+                builder
+                        .contentType(TestUtil.APPLICATION_JSON_UTF8)
+                        .content(TestUtil.convertObjectToJsonBytes(norme)))
+                .andExpect(status().isBadRequest());
     }
 
 }
