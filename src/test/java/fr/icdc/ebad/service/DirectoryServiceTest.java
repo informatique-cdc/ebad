68e1fa7aebeb7fc838d2760ebe970e488e1dd163
Damien Trouillet <damien.trouillet@caissedesdepots.fr>
2020-01-16 16:46:58 +0100

    Fixe unit test

--- src/test/java/fr/icdc/ebad/service/DirectoryServiceTest.java
+++ src/test/java/fr/icdc/ebad/service/DirectoryServiceTest.java
@@ -32,6 +32,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.notNull;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
@@ -98,27 +99,24 @@ public class DirectoryServiceTest {
 
 
     @Test(expected = EbadServiceException.class)
-    public void uploadFile() throws SftpException, JSchException, EbadServiceException {
-        InputStream inputStream = IOUtils.toInputStream("hello", Charset.forName("UTF-8"));
+    public void uploadFile() throws SftpException, JSchException, EbadServiceException, IOException {
 
         Directory directory = Directory.builder().id(1L).name("test").canWrite(true).build();
-        FilesDto filesDTO = new FilesDto();
-        filesDTO.setDirectory(directory);
-        doNothing().when(shellService).uploadFile(eq(directory), eq(inputStream), eq(filesDTO.getName()));
+        when(directoryRepository.getOne(eq(1L))).thenReturn(directory);
         MockMultipartFile secondFile = new MockMultipartFile("data", "other-file-name.data", "text/plain", "some other type".getBytes());
 
+        doNothing().when(shellService).uploadFile(eq(directory), notNull(), eq("other-file-name.data"));
+
         directoryService.uploadFile(secondFile, 1L);
-        verify(shellService).uploadFile(eq(directory), eq(inputStream), eq(filesDTO.getName()));
+        verify(shellService).uploadFile(eq(directory), notNull(), eq("other-file-name.data"));
 
         directory.setCanWrite(false);
-        doThrow(new SftpException(1, "test")).when(shellService).uploadFile(eq(directory), eq(inputStream), eq(filesDTO.getName()));
+        doThrow(new SftpException(1, "test")).when(shellService).uploadFile(eq(directory), notNull(), eq("other-file-name.data"));
         directoryService.uploadFile(secondFile, 1L);
     }
 
     @Test(expected = IllegalAccessError.class)
     public void uploadFileKo() throws EbadServiceException {
-        InputStream inputStream = IOUtils.toInputStream("hello", Charset.forName("UTF-8"));
-        FilesDto filesDTO = new FilesDto();
         MockMultipartFile secondFile = new MockMultipartFile("data", "other-file-name.data", "text/plain", "some other type".getBytes());
 
         directoryService.uploadFile(secondFile, 1L);
