8b7afb61ab298a3480c299147d5180687b13d64b
Jonathan Leitschuh <jonathan.leitschuh@gmail.com>
2020-09-21 06:00:00 -0400

    CVE-2019-16303 - JHipster Vulnerability Fix - Use CSPRNG in RandomUtil (#49)
    This fixes a security vulnerability in this project where the `RandomUtil.java`
    file(s) were using an insecure Pseudo Random Number Generator (PRNG) instead of
    a Cryptographically Secure Pseudo Random Number Generator (CSPRNG) for
    security sensitive data.
    
    Signed-off-by: Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>

--- src/main/java/fr/icdc/ebad/service/util/RandomUtil.java
+++ src/main/java/fr/icdc/ebad/service/util/RandomUtil.java
@@ -3,14 +3,25 @@ package fr.icdc.ebad.service.util;
 
 import org.apache.commons.lang3.RandomStringUtils;
 
+import java.security.SecureRandom;
+
 /**
  * Utility class for generating random Strings.
  */
 public final class RandomUtil {
+    private static final SecureRandom SECURE_RANDOM = new SecureRandom();
 
     private static final int DEF_COUNT = 20;
 
+    static {
+        SECURE_RANDOM.nextBytes(new byte[64]);
+    }
+
     private RandomUtil() {
+    }
+
+    private static String generateRandomAlphanumericString() {
+        return RandomStringUtils.random(DEF_COUNT, 0, 0, true, true, null, SECURE_RANDOM);
     }
 
     /**
@@ -19,7 +30,7 @@ public final class RandomUtil {
      * @return the generated password
      */
     public static String generatePassword() {
-        return RandomStringUtils.randomAlphanumeric(DEF_COUNT);
+        return generateRandomAlphanumericString();
     }
 
     /**
@@ -28,6 +39,6 @@ public final class RandomUtil {
      * @return the generated activation key
      */
     public static String generateActivationKey() {
-        return RandomStringUtils.randomNumeric(DEF_COUNT);
+        return generateRandomAlphanumericString();
     }
 }
