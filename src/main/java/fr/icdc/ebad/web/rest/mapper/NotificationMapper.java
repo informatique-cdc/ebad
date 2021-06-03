44f84c7f795b3ba2ac136bf7f27c70823063a35e
Damien Trouillet <contact@damien-trouillet.fr>
2021-06-03 18:50:05 +0200

    Add notification mapper to return good date and not "now" (#106)

---
+++ src/main/java/fr/icdc/ebad/web/rest/mapper/NotificationMapper.java
@@ -0,0 +1,35 @@
+package fr.icdc.ebad.web.rest.mapper;
+
+import fr.icdc.ebad.domain.Notification;
+import fr.icdc.ebad.web.rest.dto.NotificationDto;
+import ma.glasnost.orika.CustomConverter;
+import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.MappingContext;
+import ma.glasnost.orika.converter.ConverterFactory;
+import ma.glasnost.orika.metadata.Type;
+import net.rakugakibox.spring.boot.orika.OrikaMapperFactoryConfigurer;
+import org.joda.time.DateTime;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificationMapper implements OrikaMapperFactoryConfigurer {
+    @Override
+    public void configure(MapperFactory orikaMapperFactory) {
+        ConverterFactory converterFactory = orikaMapperFactory.getConverterFactory();
+        converterFactory.registerConverter("notificationDateCreatedConverter", new NotificationDateCreatedConverter());
+
+        orikaMapperFactory.classMap(Notification.class, NotificationDto.class)
+                .fieldMap("createdDate", "createdDate").converter("notificationDateCreatedConverter").add()
+                .byDefault()
+                .register();
+    }
+
+    class NotificationDateCreatedConverter extends CustomConverter<DateTime, DateTime> {
+        @Override
+        public DateTime convert(DateTime createdDate, Type<? extends DateTime> type, MappingContext mappingContext) {
+            return createdDate;
+        }
+    }
+}
+
+
