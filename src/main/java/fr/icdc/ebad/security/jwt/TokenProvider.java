13a138612419d19afd6d910c2c3d9e613d294e99
Damien Trouillet <contact@damien-trouillet.fr>
2024-08-01 19:32:10 +0200

    fix(): fix spring data

--- src/main/java/fr/icdc/ebad/security/jwt/TokenProvider.java
+++ src/main/java/fr/icdc/ebad/security/jwt/TokenProvider.java
@@ -8,7 +8,10 @@ import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.SignatureException;
 import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
+import org.bouncycastle.jcajce.provider.util.SecretKeyUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -18,7 +21,9 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.Collection;
 import java.util.Date;
 import java.util.stream.Collectors;
@@ -69,16 +74,23 @@ public class TokenProvider {
         return Jwts.builder()
             .setSubject(authentication.getName())
             .claim(AUTHORITIES_KEY, authorities)
-            .signWith(SignatureAlgorithm.HS512, secretKey)
+            .signWith(SignatureAlgorithm.HS512, Base64.getEncoder().encodeToString(secretKey.getBytes()))
             .setExpiration(validity)
             .compact();
     }
 
     public Authentication getAuthentication(String token) {
-        Claims claims = Jwts.parser()
-            .setSigningKey(secretKey)
-            .parseClaimsJws(token)
-            .getBody();
+        SecretKey secret = Keys.hmacShaKeyFor(secretKey.getBytes());
+        Claims claims =
+                Jwts.parser()
+                .verifyWith(secret)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+//        Jwts.parser()
+//            .setSigningKey(secretKey)
+//            .parseClaimsJws(token)
+//            .getBody();
 
         Collection<? extends GrantedAuthority> authorities =
             Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
@@ -92,7 +104,12 @@ public class TokenProvider {
 
     public boolean validateToken(String authToken) {
         try {
-            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(authToken);
+            SecretKey secret = Keys.hmacShaKeyFor(secretKey.getBytes());
+            Jwts.parser()
+                            .verifyWith(secret)
+                            .build()
+                            .parseSignedClaims(authToken)
+                            .getPayload();
             return true;
         } catch (SignatureException e) {
             log.info("Invalid JWT signature.");
